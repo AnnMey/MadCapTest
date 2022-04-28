7569ad5553c5e829fd815a82008876094aa48215
MadCapTest <annmey@wp.pl>
2022-04-28 23:19:23 +0200

    Purge

--- HTML5/Data/Search.js
+++
@@ -1,1 +0,0 @@
-define({r:0.5,st:'Stem',n:1,t:[0],m:[],u:['../Content/A-Lokalizacja/Lokalizacja.htm'],s:['09'],p:[['09',11],['zamieszkiwali',14]]});
--- HTML5/Resources/Scripts/require.config.js
+++
@@ -1,3 +0,0 @@
-require.config({
-    urlArgs: 't=637851768993265251'
-});
